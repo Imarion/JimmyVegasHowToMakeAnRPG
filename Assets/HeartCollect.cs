98805ea075ead6d3533e6cf042f701b241b849f8
Manu <black_falcon00@hotmail.com>
2019-10-23 14:17:28 +0200

    Episode 4: hearth pick up, sound, bushes on terrain

---
+++ Assets/HeartCollect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+
+    public int rotateSpeed = 2;
+    public AudioSource collectSound;
+    public GameObject thisHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        thisHeart.SetActive(false);
+    }
+}
