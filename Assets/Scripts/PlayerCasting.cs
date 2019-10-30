aae7304ff4309861a1ddfd4147ad52d52440b55c
Manu <black_falcon00@hotmail.com>
2019-10-30 17:45:33 +0100

    Episode 6: Raycast (Quest on noticeboard).

---
+++ Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour
+{
+    public static float DistanceFromTarget;
+    public float ToTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out Hit))
+        {
+            ToTarget = Hit.distance;
+            DistanceFromTarget = ToTarget;
+        }
+    }
+}
---
+++ Assets/Scripts/Quests/Quest001Take.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest001Take : MonoBehaviour
+{
+
+    public float TheDistance;
+    public GameObject ActionKey;
+    public GameObject ActionText;
+    public GameObject UIQuest;
+    public GameObject ThePlayer;
+    public GameObject NoticeCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    private void OnMouseOver()
+    {
+        if (TheDistance <= 3)
+        {
+            ActionKey.SetActive(true);
+            ActionText.SetActive(true);
+
+            if (Input.GetButtonDown("Action"))
+            {
+                ActionKey.SetActive(false);
+                ActionText.SetActive(false);
+                UIQuest.SetActive(true);
+                NoticeCam.SetActive(true);
+                ThePlayer.SetActive(false);
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        ActionKey.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
