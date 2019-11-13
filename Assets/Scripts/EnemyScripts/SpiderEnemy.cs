a05ef48951e86c2d35d0b44b7bae50183fe7dda0
Manu <black_falcon00@hotmail.com>
2019-11-13 15:34:08 +0100

    Episode 14: enemy (spider)

---
+++ Assets/Scripts/EnemyScripts/SpiderEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderEnemy : MonoBehaviour
+{
+    public int EnemyHealth = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void DeductPoints(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemyHealth <= 0)
+        {
+            StartCoroutine(DeathSpider());
+        }
+    }
+
+    IEnumerator DeathSpider()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}
