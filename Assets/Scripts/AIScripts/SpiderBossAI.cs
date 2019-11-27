3a6da622005bf4b50d2b408b5b58d0c1102afd19
Manu <black_falcon00@hotmail.com>
2019-11-27 10:20:02 +0100

    Episode 23 completed

---
+++ Assets/Scripts/AIScripts/SpiderBossAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderBossAI : MonoBehaviour
+{
+
+    public GameObject TheDestination;
+
+    NavMeshAgent TheAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TheAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheAgent.SetDestination(TheDestination.transform.position);
+    }
+}
