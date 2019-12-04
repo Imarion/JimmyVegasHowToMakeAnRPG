304c5dd30eed44b847b007d8d80f8ec88ab17d41
Manu <black_falcon00@hotmail.com>
2019-12-04 16:30:14 +0100

    Episode 27 done

---
+++ Assets/Scripts/MainMenu/MenuControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour
+{
+    public GameObject MenuSlider;
+    public GameObject AnywhereText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SlideMenu()
+    {
+        MenuSlider.GetComponent<Animation>().Play("MainMenuSlide");
+        Destroy(AnywhereText);
+        Destroy(this);
+    }
+}
