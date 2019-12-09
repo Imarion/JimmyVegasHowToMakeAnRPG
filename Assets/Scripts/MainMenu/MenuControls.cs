ed4606db17235969315177d964524aa8061d95bd
Manu <black_falcon00@hotmail.com>
2019-12-09 16:40:52 +0100

    Episode 28 completed

--- Assets/Scripts/MainMenu/MenuControls.cs
+++ Assets/Scripts/MainMenu/MenuControls.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
     public GameObject MenuSlider;
     public GameObject AnywhereText;
+    public GameObject ClickAnywhereButton;
+    public AudioSource menuMusic;
+    public GameObject loadingGame;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,14 @@ public class MenuControls : MonoBehaviour
     {
         MenuSlider.GetComponent<Animation>().Play("MainMenuSlide");
         Destroy(AnywhereText);
+        Destroy(ClickAnywhereButton);
         Destroy(this);
+    }
+
+    public void NewGame()
+    {
+        menuMusic.Stop();
+        loadingGame.SetActive(true);
+        SceneManager.LoadScene(1);
     }
 }
